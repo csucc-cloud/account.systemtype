ea25438f697427a17599c39ca07e8b59de396b12
csucc-cloud <sialongodavie68@gmail.com>
2026-04-05 19:03:37 +0800

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://dwvxrclaqrqathhdupdq.supabase.co';
+const supabaseAnonKey = 'sb_publishable_rxuCKHJDREFC3NbXdAtOrg_DdpUxHro';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
